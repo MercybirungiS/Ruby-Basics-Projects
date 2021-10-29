510c60c2748ef243d54e58c23cdc308bd70ab2e5
MercybirungiS <mercyssbirungi@gmail.com>
2021-10-29 15:36:10 +0300

    ruby basics

---
+++ basics.rb
@@ -0,0 +1,45 @@
+puts "I am a Victor"
+# puts prints out things on a new line 
+
+print "Hello Mercy"  
+
+#  Drawing out something on the screen 
+# interpretes line by line 
+puts "     /|"
+puts "    / |"
+puts "   /  |"
+puts "  /___|"
+
+
+#variables
+
+name ="Mercy"
+puts ("My name is  " + name)
+ age =10
+puts age
+
+# new line 
+puts "Mercy \n Birungi"
+
+#  Data types (strings,) String methods 
+puts name.upcase()
+puts name.downcase()
+word = "   akirachix   "
+# remove the white space 
+puts word.length()
+puts word.strip() 
+# if a certain character shows up in a string . It is also case sensitive 
+puts name.include? "m"
+puts name.include? "M"
+# accessing characters at their index position
+puts name [3]
+
+
+
+
+
+
+
+
+
+
